e838b3780fab52cacc5c6ac3628f02922e5ccd1d
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-05-04 22:05:37 +0100

    Try to add logic to find SysDVR and MAGIC-NS
    I'm getting an error:
    
    Traceback (most recent call last):
      File "/Users/henrybrent/Local/GitHub/usb/src/main.py", line 40, in <module>
        main()
      File "/Users/henrybrent/Local/GitHub/usb/src/main.py", line 35, in main
        print(find(SysDVR))
              ^^^^^^^^^^^^
      File "/Users/henrybrent/Local/GitHub/usb/src/main.py", line 31, in find
        return usb.core.find(idVendor=idVendor, idProduct=idProduct)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Users/henrybrent/Local/GitHub/usb/venv/lib/python3.11/site-packages/usb/core.py", line 1309, in find
        raise NoBackendError('No backend available')
    usb.core.NoBackendError: No backend available

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+import usb.core
+
+SysDVR = {
+    "ProductId": "0x4ee0",
+    "VendorId": "0x18d1",
+}
+
+MAGIC_NS = {
+    "ProductId": "0xa710",
+    "VendorId": "0x20d6",
+}
+
+
+def find(device: dict):
+    idVendor = device["VendorId"]
+    idProduct = device["ProductId"]
+    return usb.core.find(idVendor=idVendor, idProduct=idProduct)
+
+
 def main():
-    pass
+    print(find(SysDVR))
+    print(find(MAGIC_NS))
 
 
 if __name__ == "__main__":
